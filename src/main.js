7f64cd21132b6c7a3bc842e38238330511452c12
Yasaswi Poolla <yasaswi.poolla@itcrats.com>
2020-07-02 21:14:59 +0530

    basic setup vue goodtable and graphs

--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ import BootstrapVue from "bootstrap-vue";
 import Vuelidate from "vuelidate";
 import VueSidebarMenu from "vue-sidebar-menu";
 import VueApexCharts from "vue-apexcharts";
+import VueFlatPickr from "vue-flatpickr-component";
+import VueGoodTablePlugin from "vue-good-table";
+import Multiselect from "vue-multiselect";
 
 //css imports
 import "bootstrap/dist/css/bootstrap.css";
@@ -17,13 +20,19 @@ import "@fortawesome/fontawesome-free/js/all.js";
 import "&css/main.css";
 import "&css/fonts.css";
 import "&css/sidebar.css";
+import "flatpickr/dist/flatpickr.css";
+import "vue-good-table/dist/vue-good-table.css";
+import "vue-multiselect/dist/vue-multiselect.min.css";
 
 Vue.use(BootstrapVue);
 Vue.use(Vuelidate);
 Vue.use(VueSidebarMenu);
 Vue.use(VueApexCharts);
+Vue.use(VueGoodTablePlugin);
+Vue.use(VueFlatPickr);
 
 Vue.component("apexchart", VueApexCharts);
+Vue.component("multiselect", Multiselect);
 
 Vue.config.productionTip = false;
 
