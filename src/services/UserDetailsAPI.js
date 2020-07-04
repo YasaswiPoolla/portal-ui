351b881680ca26f36508b05cb8ba27db90e98711
Yasaswi Poolla <yasaswi.poolla@itcrats.com>
2020-07-04 18:54:40 +0530

    fixes

---
+++ src/services/UserDetailsAPI.js
@@ -0,0 +1,22 @@
+import API from "./API";
+import BaseAPI from "./base";
+
+class UserDetailsAPI extends BaseAPI {
+  static getBaseUrl() {
+    console.log(this.getHostUrl());
+    return this.getHostUrl();
+  }
+  static getUserDetails(data) {
+    return API.post(
+      this.getBaseUrl() + "/user-details/",
+      data,
+      this.getImgOptions()
+    );
+  }
+
+  static testingFile(data) {
+    return API.post(this.getBaseUrl(), data, this.getExtOptions());
+  }
+}
+
+export default UserDetailsAPI;
